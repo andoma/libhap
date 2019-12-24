3a7a3513ed56a0b7ffa741550941340d844349f2
Andreas Smas <andreas@lonelycoder.com>
2019-12-24 09:59:07 -0800

    Revamp example code

--- src/main.c
+++ src/main.c
@@ -25,67 +25,108 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <signal.h>
+#include <string.h>
+
 #include "hap.h"
 
+#define ACCESSORY_MANUFACTURER "Lonelycoder"
+#define ACCESSORY_MODEL        "Model1"
+#define ACCESSORY_VERSION      "1.0"
+#define ACCESSORY_PASSWORD     "224-46-688"
 
 // Example to create a light-bulb accessory
 
-static void
-logcb(void *opaque, int level, const char *message)
-{
-  fprintf(stderr, "%s\n", message);
-}
-
-
 static hap_status_t
-lb_set(void *opaque, bool on)
+lightbulb_set(void *opaque, bool on)
 {
-  bool *status = opaque;
-  *status = on;
   printf("Light builb is %s\n", on ? "ON" : "OFF");
   return HAP_STATUS_OK;
+}
+
+static hap_accessory_t *
+lightbulb_main(void)
+{
+  hap_accessory_t *ha =
+    hap_accessory_create("Lightbulb", ACCESSORY_PASSWORD, "lightbuilb.cfg",
+                         HAP_CAT_LIGHTING, ACCESSORY_MANUFACTURER,
+                         ACCESSORY_MODEL, ACCESSORY_VERSION, NULL, NULL);
+
+  hap_service_t *hs = hap_light_builb_create(NULL, lightbulb_set);
+  hap_accessory_add_service(ha, hs, 1);
+  return ha;
 }
 
 
 static hap_status_t
 rgb_set(void *opaque, float r, float g, float b)
 {
-  printf("R=%f G=%f B=%f\n", r, g, b);
+  char colorstr[64];
+  snprintf(colorstr, sizeof(colorstr),
+           ";2;%d;%d;%dm", (int)(r * 255), (int)(g * 255), (int)(b * 255));
+
+  printf("\033[48%s      \033[0m "
+         "R=%1.3f G=%1.3f B=%1.3f "
+         "\033[48%s      \033[0m\n", colorstr, r, g, b, colorstr);
   return HAP_STATUS_OK;
+}
+
+static hap_accessory_t *
+rgb_main(void)
+{
+  hap_accessory_t *ha =
+    hap_accessory_create("RGB", ACCESSORY_PASSWORD, "rgb.cfg",
+                         HAP_CAT_LIGHTING, ACCESSORY_MANUFACTURER,
+                         ACCESSORY_MODEL, ACCESSORY_VERSION, NULL, NULL);
+
+  hap_service_t *hs = hap_rgb_light_create(NULL, rgb_set);
+  hap_accessory_add_service(ha, hs, 1);
+  return ha;
+}
+
+static void
+help(void)
+{
+  printf("Usage: hap <MODE>\n");
+  printf(" MODE:\n");
+  printf("   light-bulb      On/Off light bulb\n");
+  printf("   rgb             RGB/Multicolor light\n");
+}
+
+
+static void
+ctrlc(int sig)
+{
+  return;
 }
 
 
 /**
- *
+ *",
  */
 int
 main(int argc, char **argv)
 {
-  hap_accessory_t *ha =
-    hap_accessory_create("Hello World", "224-46-688",
-                         "rgb.cfg",
-                         HAP_CAT_LIGHTING,
-                         "Lonelycoder",
-                         "Model1",
-                         "1.0",
-                         logcb, NULL);
+  hap_accessory_t * ha;
 
-  hap_service_t *hs;
-
-  if(1) {
-    hs = hap_rgb_light_create(NULL, rgb_set);
-  } else {
-    hs = hap_light_builb_create(NULL, lb_set);
+  if(argc != 2) {
+    help();
+    return 1;
   }
-  hap_accessory_add_service(ha, hs, 1);
+
+  signal(SIGINT, ctrlc);
+
+  if(!strcmp(argv[1], "light-bulb")) {
+    ha = lightbulb_main();
+  } else if(!strcmp(argv[1], "rgb")) {
+    ha = rgb_main();
+  } else {
+    help();
+    return 1;
+  }
 
   hap_accessory_start(ha);
-
-  printf("Running, press ^C to stop\n");
-
   pause();
-
   hap_accessory_destroy(ha);
   return 0;
 }
-
